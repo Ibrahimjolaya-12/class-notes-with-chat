85c2abd0e8848e23d6fdfe84015ad73846f82188
Ibrahim JolaYa <ibrahimjolaya2@gmail.com>
2026-09-22 16:54:55 +0500

    Add hidden fot room route

--- backend/src/Models/ChatRoom.js
+++ backend/src/Models/ChatRoom.js
@@ -19,6 +19,7 @@ const chatRoomSchema = new mongoose.Schema(
     participants: [
       { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     ],
+    hiddenFor: [{ type: Schema.Types.ObjectId, ref: "User" }],
   },
   { timestamps: true },
 );
